c5f939d860279bb911d3ec72f8a27c358631bc48
zw <11019537@qq.com>
2019-05-22 18:16:10 +0800

    ..

--- Number/src/excel/Main.java
+++ Number/src/excel/Main.java
@@ -24,8 +24,7 @@ import jxl.write.WritableCellFormat;
 import jxl.write.WritableFont;
 import jxl.write.WritableSheet;
 import jxl.write.WritableWorkbook;
-import jxl.write.WriteException;
-import jxl.write.biff.RowsExceededException;
+import jxl.*;
 
 public class Main {
 	static File logfile;
@@ -35,7 +34,7 @@ public class Main {
 	static int month = c.get(Calendar.MONTH);
 	static int date = c.get(Calendar.DATE);
 	private static ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
-
+	static File outFileParentFile2 = null;
 	public static void main(String[] args) {
 		logfile = new File("E:" + File.separator + "match" + File.separator + "time2.txt");
 		if (!logfile.exists()) {
@@ -46,25 +45,20 @@ public class Main {
 				e.printStackTrace();
 			}
 		}
-		CopyOnWriteArrayList<File> numberFiles = Utils
-				.getFileList("E:" + File.separator + "match" + File.separator + "number");
-
+		CopyOnWriteArrayList<File> numberFiles=new CopyOnWriteArrayList<>();
+		Utils.getFileList(numberFiles,"E:" + File.separator + "match" + File.separator + "number");
 		if (numberFiles.isEmpty()) {
 			Utils.print("不存在number表！........退出", logfile);
 		} else {
-			Utils.print("开始录入number表的数据！", logfile);
 			for (File numberfile : numberFiles) {
+				Utils.print("开始录入number表的数据！", logfile);
 				List<ModelName> modelNames = startFindname(numberfile);
 				if (!modelNames.isEmpty()) {
 					Utils.print("录入name表" + numberfile.getName() + "数据完毕！共有" + modelNames.size() + "个数据", logfile);
 				}
-				List<File> officalFiles = Utils.getFileList("E:" + File.separator + "match" + File.separator + "offical");
-				SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-				Date date=new Date();
-				File outFileParentFile=new File("E:" + File.separator + "match" + File.separator+format.format(date));
-				outFileParentFile.mkdirs();
-				File outfile = new File(outFileParentFile,numberfile.getName().replaceAll(".xls", "") + "out");
-				outfile.mkdirs();
+				CopyOnWriteArrayList<File> officalFiles=new CopyOnWriteArrayList<>();
+				Utils.getFileList(officalFiles,"E:" + File.separator + "match" + File.separator + "offical");
+				initFile();
 				for (File file : officalFiles) {
 					List<Result> results = new ArrayList<>();
 					List<ModelOffical> modelOfficals = new ArrayList<>();
@@ -79,11 +73,7 @@ public class Main {
 								continue;
 							}
 							if (modelName.getExamcode().equalsIgnoreCase(modelOffical.getExamcode())) {
-								Result result = new Result(modelOffical.name, modelName.mobile,
-										"未报名,学员  " + modelOffical.name + " 招考岗位 ：" + modelOffical.aginent
-												+ modelOffical.findjob + " ,职位代码：" + modelOffical.finjobcode,
-										"");
-								results.add(result);
+								extractResult(results, modelOffical, modelName);
 							}
 						}
 					}
@@ -91,70 +81,7 @@ public class Main {
 						cachedThreadPool.execute(new Runnable() {
 							@Override
 							public void run() {
-								File fresultfile1 = new File(outfile, file.getName().replaceAll(".xls", "") + "1.xls");
-								System.out.println("fresultfile1:" + fresultfile1.getAbsolutePath());
-								if (!fresultfile1.exists()) {
-									try {
-										fresultfile1.createNewFile();
-									} catch (IOException e) {
-										e.printStackTrace();
-									}
-								}
-
-								Utils.print(file.getName() + "匹配到" + results.size() + "个数据,开始导出", logfile);
-								a = new Date();
-								int number = results.size();
-								if (number > 0) {
-									if (number > 3) {
-										int plus = 0;
-										if (number % 3 > 0) {
-											plus = (number - number % 3) / 3;
-										} else {
-											plus = number / 3;
-										}
-										List<Result> list1 = new ArrayList<>();
-										List<Result> list2 = new ArrayList<>();
-										List<Result> list3 = new ArrayList<>();
-										List<Result> list4 = new ArrayList<>();
-										for (int i = 0; i < plus; i++) {
-											list1.add(results.get(i));
-										}
-										for (int i = plus; i < plus * 2; i++) {
-											list2.add(results.get(i));
-										}
-										for (int i = plus * 2; i < plus * 3; i++) {
-											list3.add(results.get(i));
-										}
-
-										exportExcel(fresultfile1.getAbsolutePath(), list1);
-
-										File fresultfile2 = new File(outfile,
-												file.getName().replaceAll(".xls", "") + "2.xls");
-										if (!fresultfile2.exists()) {
-											try {
-												fresultfile2.createNewFile();
-											} catch (IOException e) {
-												e.printStackTrace();
-											}
-										}
-
-										File fresultfile3 = new File(outfile,
-												file.getName().replaceAll(".xls", "") + "3.xls");
-										if (!fresultfile3.exists()) {
-											try {
-												fresultfile3.createNewFile();
-											} catch (IOException e) {
-												e.printStackTrace();
-											}
-										}
-
-										exportExcel(fresultfile2.getAbsolutePath(), list2);
-										exportExcel(fresultfile3.getAbsolutePath(), list3);
-
-									} else {
-										exportExcel(fresultfile1.getAbsolutePath(), results);
-									}
-								}
+								ectractFile(file, results);
 							}
 						});
 
@@ -169,7 +96,14 @@ public class Main {
 			}
 		}
 	}
-
+	private static void extractResult(List<Result> results, ModelOffical modelOffical, ModelName modelName) {
+		Result result = new Result(modelOffical.name, modelName.mobile,
+				"未报名,学员  " + modelOffical.name + " 招考岗位 ：" + modelOffical.aginent + modelOffical.findjob + " ,职位代码："
+						+ modelOffical.finjobcode + "  考了" + modelOffical.finalscore + "分  " + "  招: "
+						+ modelOffical.finjobnumber + "人  " + "排:" + modelOffical.rank + "名  ",
+				" ");
+		results.add(result);
+	}
 	// 把name表的数据录进来
 	private static List<ModelName> startFindname(File file) {
 		InputStream inputStream = null;
@@ -228,12 +162,23 @@ public class Main {
 				if (excamcode != -1) {
 					modelName.setExamcode(sheet.getCell(excamcode, i).getContents());
 				}
-
 				modelNames.add(modelName);
 			}
 		}
 		return modelNames;
 
+	}
+	
+	private static void initFile() {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH时" + "mm分");
+		Date date = new Date();
+		String timeString = format.format(date);
+		File outFileParentFile = new File("E:" + File.separator + "match" + File.separator + "numberout");
+		if (!outFileParentFile.exists()) {
+			outFileParentFile.mkdir();
+		}
+		outFileParentFile2 = new File(outFileParentFile, timeString);
+		outFileParentFile2.mkdirs();
 	}
 
 	private static List<ModelOffical> findOffical(File file) {
@@ -247,7 +192,7 @@ public class Main {
 			int number = 0;
 			if (workbook.getNumberOfSheets() == 1) {
 				number = 0;
-				modelOfficals = findOfficalSheet(file, workbook.getSheet(0), true);
+				modelOfficals = findOfficalSheet(file, workbook, 0);
 				System.out.println("--------------找到官方表" + file.getName() + "在第" + number + "个子表");
 			} else {
 				String names[] = workbook.getSheetNames();
@@ -255,7 +200,7 @@ public class Main {
 				for (int i = 0; i < names.length; i++) {
 					System.out.println("表单名:" + names[i]);
 					number = i;
-					modelOfficals.addAll(findOfficalSheet(file, workbook.getSheet(number), false));
+					modelOfficals.addAll(findOfficalSheet(file, workbook, number));
 					System.out.println("--------------找到官方表" + file.getName() + "在第" + number + "个子表");
 				}
 			}
@@ -265,14 +210,18 @@ public class Main {
 		return modelOfficals;
 	}
 
-	private static List<ModelOffical> findOfficalSheet(File file, Sheet sheet, boolean b) {
+	private static List<ModelOffical> findOfficalSheet(File file, Workbook workbook, int number) {
+
+		Sheet sheet = workbook.getSheet(number);
 		List<ModelOffical> modelOfficals = new ArrayList<>();
 		System.out.println("Colums:" + sheet.getColumns() + "Rows:" + sheet.getRows());
 		int agent = -1, findjob = -1, findjobcode = -1, findjobnumber = -1, name = -1, examcode = -1, xingcescore = -1,
-				shenglunscore = -1, finalscore = -1, rank = -1, university = -1, danwei = -1;
-		int total = 0;
+				shenglunscore = -1, finalscore = -1, rank = -1;
 		int row = -1;
 		int finalrow = sheet.getRows();
+		if (sheet.getRows()==0|| sheet.getColumns()==0) {
+			return modelOfficals;
+		}
 		for (int j = 0; j < sheet.getRows(); j++) {
 			for (int i = 0; i < sheet.getColumns(); i++) {
 				String Columname = sheet.getCell(i, j).getContents();
@@ -310,36 +259,39 @@ public class Main {
 						|| Columname.contains("排名")) {
 					row = j;
 					rank = i;
-				} else if (Columname.contains("毕业院校")) {
-					row = j;
-					university = i;
-				} else if (Columname.contains("工作单位")) {
-					row = j;
-					danwei = i;
-				} else if (Columname.contains("备注：")) {
-					row = j;
 				}
-				if (findjob != -1 && findjobcode != -1 && name != -1) {
+				if (findjob != -1 && findjobcode != -1 && name != -1 && finalscore != -1 && rank != -1
+						&& findjobnumber != -1) {
 					break;
 				}
 			}
-			if (findjob != -1 && findjobcode != -1 && name != -1) {
+			if (findjob != -1 && findjobcode != -1 && name != -1 && finalscore != -1 && rank != -1
+					&& findjobnumber != -1) {
 				break;
 			}
 		}
-		if (!(agent == -1 && findjob == -1 && findjobcode == -1 && name == -1)) {
+		System.out.println(findjob+" "+findjobcode+" "+name+" "+finalscore+" "+rank+" "+findjobnumber);
+		if (!(findjob != -1 && findjobcode != -1 && name != -1 && finalscore != -1 && rank != -1
+				&& findjobnumber != -1)) {
 			if (findjob == -1) {
 				Utils.print(file.getName() + "Offical表找不到招录职位,请改为招录职位", logfile);
 			}
 			if (findjobcode == -1) {
 				Utils.print(file.getName() + "Offical表找不到职位代码,请改为职位代码", logfile);
 			}
-			if (findjobnumber == -1) {
-				Utils.print(file.getName() + "Offical表找不到招录计划,请改为招录计划或招考人数", logfile);
-			}
 			if (name == -1) {
-				Utils.print(file.getName() + "Offical表找不到准考证号,请改为 准考证号或 考号", logfile);
+				Utils.print(file.getName() + "Offical表找不到名字,请改为名字", logfile);
 			}
+			if (finalscore == -1) {
+				Utils.print(file.getName() + "Offical表找不到分数,请改为折算分", logfile);
+			}
+			if (rank == -1) {
+				Utils.print(file.getName() + "Offical表找不到排名,请改为排名", logfile);
+			}
+			if (findjobnumber == -1) {
+				Utils.print(file.getName() + "Offical表找不到招录计划,请改为职位代码", logfile);
+			}
+
 		} else {
 			for (int j = row + 1; j < finalrow; j++) {
 				ModelOffical book = new ModelOffical();
@@ -401,6 +353,7 @@ public class Main {
 		}
 		return modelOfficals;
 
+	
 	}
 
 	public synchronized static void exportExcel(String fileName, List<Result> list) {
@@ -427,8 +380,7 @@ public class Main {
 			WritableCellFormat wcf2 = new WritableCellFormat(wf2);
 			wcf2.setVerticalAlignment(VerticalAlignment.CENTRE);
 			wcf2.setAlignment(Alignment.CENTRE);
-			ws.setRowView(1, 500);
-
+			
 			jxl.write.Label jlabel1 = new jxl.write.Label(0, 0, "姓名");
 			jxl.write.Label jlabel2 = new jxl.write.Label(1, 0, "手机号码");
 			jxl.write.Label jlabel3 = new jxl.write.Label(2, 0, "描述");
@@ -473,5 +425,71 @@ public class Main {
 			e.printStackTrace();
 		}
 
+	}
+	private static void ectractFile(File file, List<Result> results) {
+		File fresultfile1 = new File(outFileParentFile2, file.getName().replaceAll(".xls", "") + "1.xls");
+		System.out.println("fresultfile1:" + fresultfile1.getAbsolutePath());
+		if (!fresultfile1.exists()) {
+			try {
+				fresultfile1.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		Utils.print(file.getName() + "匹配到" + results.size() + "个数据,开始导出", logfile);
+		a = new Date();
+		int number = results.size();
+		if (number > 0) {
+			if (number > 3) {
+				int plus = 0;
+				if (number % 3 > 0) {
+					plus = (number - number % 3) / 3;
+				} else {
+					plus = number / 3;
+				}
+				List<Result> list1 = new ArrayList<>();
+				List<Result> list2 = new ArrayList<>();
+				List<Result> list3 = new ArrayList<>();
+				List<Result> list4 = new ArrayList<>();
+				for (int i = 0; i < plus; i++) {
+					list1.add(results.get(i));
+				}
+				for (int i = plus; i < plus * 2; i++) {
+					list2.add(results.get(i));
+				}
+				for (int i = plus * 2; i < plus * 3; i++) {
+					list3.add(results.get(i));
+				}
+
+				exportExcel(fresultfile1.getAbsolutePath(), list1);
+
+				File fresultfile2 = new File(outFileParentFile2,
+						file.getName().replaceAll(".xls", "") + "2.xls");
+				if (!fresultfile2.exists()) {
+					try {
+						fresultfile2.createNewFile();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+
+				File fresultfile3 = new File(outFileParentFile2,
+						file.getName().replaceAll(".xls", "") + "3.xls");
+				if (!fresultfile3.exists()) {
+					try {
+						fresultfile3.createNewFile();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+
+				exportExcel(fresultfile2.getAbsolutePath(), list2);
+				exportExcel(fresultfile3.getAbsolutePath(), list3);
+
+			} else {
+				exportExcel(fresultfile1.getAbsolutePath(), results);
+			}
+		}
 	}
 }
--- Number/src/excel/Utils.java
+++ Number/src/excel/Utils.java
@@ -14,29 +14,26 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Utils {
-	public static CopyOnWriteArrayList<File> filelist = new CopyOnWriteArrayList<File>();
+	
 
-	public static CopyOnWriteArrayList<File> getFileList(String strPath) {
+	public static void getFileList(CopyOnWriteArrayList<File> filelist,String strPath) {
 		File dir = new File(strPath);
 		File[] files = dir.listFiles(); // 该文件目录下文件全部放入数组
 		if (files != null) {
 			for (int i = 0; i < files.length; i++) {
 				String fileName = files[i].getName();
 				if (files[i].isDirectory()) { // 判断是文件还是文件夹
-					getFileList(files[i].getAbsolutePath()); // 获取文件绝对路径
+					getFileList(filelist,files[i].getAbsolutePath()); // 获取文件绝对路径
 				} else if (fileName.endsWith("xls")) { // 判断文件名是否以.avi结尾
 					String strFileName = files[i].getAbsolutePath();
 					System.out.println("-------" + strFileName);
 					filelist.add(files[i]);
-
 				} else {
 					continue;
 				}
 			}
 
 		}
-
-		return filelist;
 	}
 
 	// 打印日志
